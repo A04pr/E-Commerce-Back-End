b2a16a4f3c66ab3d6c78678e84634f5b9eb47b65
Aidan Reed <areed041704@gmail.com>
2024-02-09 12:25:00 -0500

    finished models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -12,6 +12,25 @@ const ProductTag = require('./ProductTag');
 
 // Tags belongToMany Products (through ProductTag)
 
+Category.belongsTo(Product, {
+  foreignKey: 'id',
+});
+
+Category.hasMany(Product, {
+  foreignKey: 'player_id',
+  onDelete: 'CASCADE',
+});
+
+
+Product.belongToMany(Tag, {
+  foreignKey: 'id',
+  onDelete: 'CASCADE',
+});
+
+Tag.belongToMany(Product, {
+  foreignKey: 'id',
+});
+
 module.exports = {
   Product,
   Category,
