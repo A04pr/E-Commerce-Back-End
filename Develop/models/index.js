7b246f1430dcd4be8eeb95d13914c56558b9e8a1
A04pr <areed041704@gmail.com>
2024-02-10 07:27:34 -0700

    corrected models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,30 +5,30 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: 'ProductTag',
+  foreignKey: 'product_id',
+  otherKey: 'tag_id',
+  onDelete: 'CASCADE',
+});
 
 // Tags belongToMany Products (through ProductTag)
-
-Category.belongsTo(Product, {
-  foreignKey: 'id',
-});
-
-Category.hasMany(Product, {
-  foreignKey: 'player_id',
+Tag.belongsToMany(Product, {
+  through: 'ProductTag',
+  foreignKey: 'tag_id',
+  otherKey: 'product_id', 
   onDelete: 'CASCADE',
-});
-
-
-Product.belongToMany(Tag, {
-  foreignKey: 'id',
-  onDelete: 'CASCADE',
-});
-
-Tag.belongToMany(Product, {
-  foreignKey: 'id',
 });
 
 module.exports = {
